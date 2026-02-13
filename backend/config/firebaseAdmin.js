50716ddb48869b2da41905b0c416f104f34046e9
Karan Dutt <karandutta2011@gmail.com>
2026-02-13 11:08:21 +0530

    issue-1: Adding the comment for th if condition

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,6 +1,10 @@
 const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccountKey.json");
 
+/* 
+  If no Firebase app has been created yet, create one. 
+  This is important to prevent "Firebase app already exists" errors when this module is imported multiple times.
+*/
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
